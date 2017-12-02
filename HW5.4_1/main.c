f1937dd97c248169d26d97605fb11ea83880b801
dima <dima@acerV5>
2017-12-02 15:49:14 +0300

    first commit

---
+++ HW5.4_1/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_INPUT_SIZE 512
+
+int main(int argc, char* argv[]) {
+    const char outFileName[] = "out";
+    char input[MAX_INPUT_SIZE];
+    char cmd[MAX_INPUT_SIZE];
+
+    FILE* cmdFile;
+    FILE* outFile;
+
+    if(fgets(cmd, sizeof(cmd), stdin) == NULL) {
+        fprintf(stderr, "Invalid input from stdin\n");
+        exit(EXIT_FAILURE);
+    }
+
+    cmdFile = popen(cmd, "r");
+    if(cmdFile == NULL) {
+        fprintf(stderr, "popen() failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    outFile = fopen(outFileName, "w");
+    if(outFile == NULL) {
+        fprintf(stderr, "fopen() failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while(fgets(input, sizeof(input), cmdFile) != NULL) {
+        fprintf(outFile, input);
+    }
+
+    fclose(outFile);
+    pclose(cmdFile);
+    exit(EXIT_SUCCESS);
+}
